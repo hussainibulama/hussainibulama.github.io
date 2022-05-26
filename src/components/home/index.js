34f459d31dc8cfeea4a7bcf785cbbf4449fce713
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2022-05-26 19:32:38 +0100

    Brief donw

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -19,16 +19,42 @@ function Home() {
           </div>
           <div className="intro">
             <p>
-              I’m a software engineer specializing in building (and occasionally
-              designing) exceptional digital experiences. Currently, I’m focused
-              on building accessible, human-centered products at{" "}
-              <span>Upstatement</span>.
+              I’m a software engineer with startup experience. I run a startup
+              for a year or so before quiting to join others and learn from
+              them. I always build and love building amazing product that solves
+              complex problems using agile methodology. For now, I work mostly
+              as a <span>Full Stack Engineer </span>
+              and/or <span>DevOps Engineer(AWS)/System Architect.</span>
             </p>
           </div>
           <div>
             <a className="but" href="linked.com">
               Check me out on linkedIn
             </a>
+          </div>
+        </div>
+        <div className="level2">
+          <div className="header">
+            <h1>About Me</h1>
+          </div>
+          <div className="others">
+            <div className="comment">
+              <p>
+                Hello! My name is Brittany and I enjoy creating things that live
+                on the internet. My interest in web development started back in
+                2012 when I decided to try editing custom Tumblr themes — turns
+                out hacking together a custom reblog button taught me a lot
+                about HTML & CSS! Fast-forward to today, and I’ve had the
+                privilege of working at an advertising agency, a start-up, a
+                huge corporation, and a student-led design studio. My main focus
+                these days is building accessible, inclusive products and
+                digital experiences at Upstatement for a variety of clients. I
+                also recently launched a course that covers everything you need
+                to build a web app with the Spotify API using Node & React. Here
+                are a few technologies I’ve been working with recently:
+              </p>
+            </div>
+            <div className="picture_side"></div>
           </div>
         </div>
       </div>
